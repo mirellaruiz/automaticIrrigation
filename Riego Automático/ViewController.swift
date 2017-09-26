93df0405729edca5c27dc161a019af16e5e0c97e
g743 DIT UPM <g743@l137.local>
2017-09-26 15:06:19 +0200

    Initial Commit

---
+++ Riego Automático/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Riego Automático
+//
+//  Created by g743 DIT UPM on 26/9/17.
+//  Copyright © 2017 g743 DIT UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Riego AutomáticoTests/Riego_Automa_ticoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Riego_Automa_ticoTests.swift
+//  Riego AutomáticoTests
+//
+//  Created by g743 DIT UPM on 26/9/17.
+//  Copyright © 2017 g743 DIT UPM. All rights reserved.
+//
+
+import XCTest
+@testable import Riego_Automa_tico
+
+class Riego_Automa_ticoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
